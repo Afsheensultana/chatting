65e86f51568590917116064dd072b46709648228
Mohammad Afsheen <mohammadafsheen@Mohammads-Air.mshome.net>
2024-02-09 23:44:33 +0530

    intial commit

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+PRIVATE_KEY="f663a535-ca30-4cb3-b089-e48fc38cb6e6"
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "private-key": PRIVATE_KEY }
+    )
+    return response.json()
+
+
+
